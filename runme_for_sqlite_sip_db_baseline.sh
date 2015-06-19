5b31a19af9a8f7a4390d3535c9b41b20e18ccc54
martinhecher <martin.hecher@fraunhofer.at>
2015-06-19 16:20:55 +0200

    removed obsolete files in preparation for updating the SIP Generator

--- runme_for_sqlite_sip_db_baseline.sh
+++
@@ -1,1 +0,0 @@
-sqlite3 sip_db.db < server/executables/sipgen/SIP_Generator/eArdDB_20130411_0809_sqlite.sql
--- server/executables/droid/runme_folder.sh.bat
+++
@@ -1,1 +0,0 @@
-java -jar droid-command-line-6.2.0-SNAPSHOT.jar -q -R -Nr src -Ns DROID_SignatureFile_V74.xml
