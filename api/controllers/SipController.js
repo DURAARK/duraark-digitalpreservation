37196f710dc30b66796233bba942c36f0db154d7
Christian Caldera <christian.caldera@fraunhofer.at>
2015-06-26 13:33:54 +0200

    added possibility for multiple directories

--- api/controllers/SipController.js
+++ api/controllers/SipController.js
@@ -1,78 +1,72 @@
 /**
  * SipController
  *
- * @description :: Server-side logic for managing sips
+ * @description :: Server-side logic for managing SIP
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 var RDFTranslator = require('../../bindings/rdf-translator/app'),
     fs = require('fs'),
     path = require('path'),
-    cwd = process.cwd();
+    mkdirp = require('mkdirp');
+
+var homeDir = '/tmp'
 
 module.exports = {
     create: function(req, res, next) {
-        var body = req.body,
-            session = body.sessions[0],
-            physicalAsset = session.physicalAssets[0],
-            buildm = session.buildm[0],
-            sessionPath = path.join(homeDir, 'session_' + path.basename(buildm['@id'])),
-            sipPath = path.join(sessionPath, 'ie_id1'),
-            masterPath = path.join(sipPath, 'master'),
-            derivativePath = path.join(sipPath, 'derivative_copy'),
-            sourceMDPath = path.join(sipPath, 'sourcemd');
+        var body = req.body;
+        var session = body.sessions[0];
+        var physicalAsset = session.physicalAssets[0];
+        var buildm = session.buildm[0];
+
+        var sessionPath = path.join(homeDir, 'session_' + path.basename(buildm['@id']));
+
+        _.forEach(physicalAsset.digitalObjects, function(da, key) {
+            var folder = key + 1;
+            var sipPath = path.join(sessionPath, 'ie_id' + folder);
+            var masterPath = path.join(sipPath, 'master');
+            var derivativePath = path.join(sipPath, 'derivative_copy');
+            var sourceMDPath = path.join(sipPath, 'sourcemd');
+
+            //create needed directories
+            console.log("ConverterControler::Creating directories");
+            mkdirp(masterPath, function(err) {
+                mkdirp(derivativePath, function(err) {
+                    mkdirp(sourceMDPath, function(err) {
+                        console.log("ConverterControler::Symlink to IFC");
 
 
-        //create needed directories
-        console.log("ConverterControler::Creating directories");
-        mkdirp(masterPath, function(err) {
-            mkdirp(derivativePath, function(err) {
-                mkdirp(sourceMDPath, function(err) {
-                    console.log("ConverterControler::Symlink to IFC");
+                        var sourceIFCPath = da.path;
+                        var targetIFCPath = path.join(masterPath, path.basename(sourceIFCPath));
 
-                    var sourceIFCPath = physicalAsset.digitalObjects[0].path;
-                    var targetIFCPath = path.join(masterPath, path.basename(sourceIFCPath));
+                        fs.link(sourceIFCPath, targetIFCPath, function() {
+                            _.forEach(da.derivatives, function(n, key) {
+                                console.log("ConverterControler::Symlink to derivates");
 
-                    //temp for files todo delete:
-                    console.log("ConverterControler::Temp: redirect file: " + sourceIFCPath + " -> " + targetIFCPath);
-                    sourceIFCPath = path.join(homeDir, "reviewForm.html");
-                    targetIFCPath = path.join(masterPath, path.basename(physicalAsset.digitalObjects[0].path));
-                    //enddelete
+                                var derivatesSourceFilePath = n.path;
+                                var derivatesTargetFilePath = path.join(derivativePath, path.basename(derivatesSourceFilePath));
 
-                    fs.link(sourceIFCPath, targetIFCPath, function() {
-                        _.forEach(physicalAsset.digitalObjects[0].derivatives, function(n, key) {
-                            console.log("ConverterControler::Symlink to derivates");
+                                fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
 
-                            var derivatesSourceFilePath = n.path;
-                            var derivatesTargetFilePath = path.join(derivativePath, path.basename(derivatesSourceFilePath));
-
-                            //temp for files todo delete:
-                            console.log("ConverterControler::Temp: redirect file: " + derivatesSourceFilePath + " -> " + derivatesTargetFilePath);
-                            derivatesSourceFilePath = path.join(homeDir, "reviewForm.html");
-                            derivatesTargetFilePath = path.join(derivativePath, path.basename(n.path));
-                            //enddelete
-
-
-                            fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
-
+                                });
                             });
-                        });
 
 
-                        console.log("ConverterControler::Createing buildm.xml");
-                        var rdf = new RDFTranslator();
+                            console.log("ConverterControler::Createing buildm.xml");
+                            var rdf = new RDFTranslator();
 
-                        rdf.extractFromJSONLD(buildm, "json-ld", "xml", function(data) {
-                            console.log("ConverterControler::Writing buildm.xml");
+                            rdf.extractFromJSONLD(buildm, "json-ld", "xml", function(data) {
+                                console.log("ConverterControler::Writing buildm.xml");
 
-                            fs.writeFile(path.join(sourceMDPath, 'buildm.xml'), data, function(err) {
-                                res.send(data);
+                                fs.writeFile(path.join(sourceMDPath, 'buildm.xml'), data, function(err) {
+                                    res.send(data);
+                                });
                             });
-                        });
 
+                        });
                     });
                 });
             });
         });
     }
-};
+}
