4eb9d004c74a5079fbe58b2eb3ea094fffa843a9
Christian Caldera <christian.caldera@fraunhofer.at>
2015-07-02 16:00:12 +0200

    added bagit and created promise library in sigenerator

--- api/controllers/SipController.js
+++ api/controllers/SipController.js
@@ -5,12 +5,90 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-var RDFTranslator = require('../../bindings/rdf-translator/app'),
-    fs = require('fs'),
+var Promise = require("bluebird"),
+    RDFTranslator = require('../../bindings/rdf-translator/app'),
+    BagIt = require('../../bindings/bagit/app'),
+    fs = Promise.promisifyAll(require('fs')),
     path = require('path'),
-    mkdirp = require('mkdirp');
+    mkdirp = require('mkdirp'),
+    _ = require('lodash');
 
-var homeDir = '/tmp'
+var homeDir = '/tmp' ;
+
+function newFolderStructure(opts) {
+  return new Promise(function(resolve, reject) {
+    console.log("ConverterControler::Creating directories");
+
+    mkdirp(opts.masterPath, function(err) {
+        mkdirp(opts.derivativePath, function(err) {
+            mkdirp(opts.sourceMDPath, function(err) {
+                resolve(opts);
+              });
+        });
+    });
+  });
+
+}
+
+function symLinkToIFC(opts) {
+  return new Promise(function(resolve, reject) {
+
+    console.log("ConverterControler::Symlink to IFC");
+
+    var sourceIFCPath = opts.digitalObject.path;
+    var targetIFCPath = path.join(opts.masterPath, path.basename(sourceIFCPath));
+
+
+    fs.link(sourceIFCPath, targetIFCPath, function() {
+      //console.log(JSON.stringify(opts.digitalObject.derivatives, null, 4));
+      resolve(opts);
+    });
+  });
+}
+
+function symLinktoDerivates(opts) {
+
+  return Promise.each(opts.digitalObject.derivatives, function (derivativeObject) {
+      return new Promise(function(resolve, reject) {
+
+        console.log("ConverterControler::Symlink to derivates");
+
+            var derivatesSourceFilePath = derivativeObject.path;
+            var derivatesTargetFilePath = path.join(opts.derivativePath, path.basename(derivatesSourceFilePath));
+
+            fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
+              resolve();
+            });
+        });
+    }).then(function () {
+      return opts;
+    });
+}
+
+function createBuilMXML(opts) {
+  return new Promise(function(resolve, reject) {
+
+
+  console.log("ConverterControler::Creating buildm.xml");
+  var rdf = new RDFTranslator();
+
+  rdf.extractFromJSONLD(opts.buildm, "json-ld", "xml", function(data) {
+      console.log("ConverterControler::Writing buildm.xml");
+
+
+      fs.writeFile(path.join(opts.sourceMDPath, 'buildm.xml'), data, function(err) {
+        resolve();
+      });
+  });
+});
+}
+
+function createBagIt() {
+  var bagit = new BagIt('/tmp/session_physicalasset_d86c761c42e440659a8a5b945f695b76', '/tmp/bag.zip');
+  bagit.bagIt(function(){
+  });
+
+}
 
 module.exports = {
     create: function(req, res, next) {
@@ -33,7 +111,46 @@ module.exports = {
 
         var sessionPath = path.join(homeDir, 'session_' + path.basename(pa[0]['@id']));
 
-        _.forEach(physicalAsset.digitalObjects, function(da, key) {
+        Promise.each(physicalAsset.digitalObjects, function(digitalObject, index, value){
+
+          var folder = index + 1;
+          var sipPath = path.join(sessionPath, 'ie_id' + folder);
+
+          var opts = {
+           buildm : buildm,
+           digitalObject: digitalObject,
+           sipPath : sipPath,
+           masterPath : path.join(sipPath, 'master'),
+           derivativePath : path.join(sipPath, 'derivative_copy'),
+           sourceMDPath : path.join(sipPath, 'sourcemd'),
+          };
+
+
+          return newFolderStructure(opts)
+          .then(symLinkToIFC)
+          .then(symLinktoDerivates)
+          .then(createBuilMXML);
+        }).then(createBagIt);
+
+
+/*
+          return newFolderStructure(opts)
+          .then(function (){
+            return symLinkToIFC(opts);
+          }).then(function (derivatives){
+            return Promise.each(derivatives, function (derivativeObject) {
+              return symLinktoDerivates(derivativeObject, opts);
+            });
+          }).then(function () {
+            return createBuilMXML(opts);
+          });
+        }).then(function () {
+          return createBagIt();
+        });
+
+
+*/
+        /*_.forEach(physicalAsset.digitalObjects, function(da, key) {
             var folder = key + 1;
             var sipPath = path.join(sessionPath, 'ie_id' + folder);
             var masterPath = path.join(sipPath, 'master');
@@ -47,13 +164,12 @@ module.exports = {
                     mkdirp(sourceMDPath, function(err) {
                         console.log("ConverterControler::Symlink to IFC");
 
-
                         var sourceIFCPath = da.path;
                         var targetIFCPath = path.join(masterPath, path.basename(sourceIFCPath));
 
                         fs.link(sourceIFCPath, targetIFCPath, function() {
+                            console.log("ConverterControler::Symlink to derivates");
                             _.forEach(da.derivatives, function(n, key) {
-                                console.log("ConverterControler::Symlink to derivates");
 
                                 var derivatesSourceFilePath = n.path;
                                 var derivatesTargetFilePath = path.join(derivativePath, path.basename(derivatesSourceFilePath));
@@ -63,7 +179,6 @@ module.exports = {
                                 });
                             });
 
-
                             console.log("ConverterControler::Createing buildm.xml");
                             var rdf = new RDFTranslator();
 
@@ -71,7 +186,9 @@ module.exports = {
                                 console.log("ConverterControler::Writing buildm.xml");
 
                                 fs.writeFile(path.join(sourceMDPath, 'buildm.xml'), data, function(err) {
-                                    res.send(data);
+                                  //return resolve();
+                                    //res.send(data);
+
                                 });
                             });
 
@@ -79,6 +196,13 @@ module.exports = {
                     });
                 });
             });
-        });
+        }).then(function(){
+
+          console.log("ConverterControler::bag it");
+          var bagit = new BagIt('/tmp/session_physicalasset_d86c761c42e440659a8a5b945f695b76', '/tmp/bag.zip');
+          bagit.bagIt(function(){
+
+          });
+        });*/
     }
-}
+};
---
+++ bindings/bagit/app.js
@@ -0,0 +1,49 @@
+var spawn = require('child_process').spawn,
+    uuid = require('node-uuid'),
+    path = require('path'),
+    fs = require('fs'),
+    mkdirp = require('mkdirp'),
+    _ = require('lodash');
+
+var sourceDir;
+var output;
+var bagItExecutable = path.join(__dirname, '../../app/bagit-4.9.0/bin');
+/**
+ * Provides NodeJS-Javascript bindings for the 'orthogen' executable.
+ *
+ * @module widget
+ */
+var BagIt = module.exports = function(sourceDir, output) {
+  this.sourceDir = sourceDir;
+  this.output = output;
+};
+
+BagIt.prototype.bagIt = function(cb) {
+
+    console.log('[BagIt::createElecDetection] configuration: ' + JSON.stringify(this.output, null, 4));
+
+    // TODO: change to session directory here?
+    var cwd = process.cwd();
+
+    process.chdir(bagItExecutable);
+
+      var arguments = ['create', this.output,
+        this.sourceDir, '--writer', 'zip'
+      ];
+
+      var executable = spawn(path.join(bagItExecutable, 'bag.bat '), arguments);
+
+      executable.stdout.on('data', function(data) {
+          console.log(data.toString());
+      });
+
+      executable.stderr.on('data', function(data) {
+          console.log('ERROR: ' + data.toString());
+      });
+
+      executable.on('close', function(code) {
+          console.log('[BagIt-binding] child process exited with code ' + code);
+
+          cb();
+      }.bind(this));
+};
