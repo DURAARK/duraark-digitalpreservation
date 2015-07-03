1a816eaef0f1cd28d27255fe09d1ed4cd88f64e9
Christian Caldera <christian.caldera@fraunhofer.at>
2015-07-03 09:26:07 +0200

    beautify code

--- api/controllers/SipController.js
+++ api/controllers/SipController.js
@@ -1,17 +1,17 @@
 /**
- * SipController
- *
- * @description :: Server-side logic for managing SIP
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
+* SipController
+*
+* @description :: Server-side logic for managing SIP
+* @help        :: See http://links.sailsjs.org/docs/controllers
+*/
 
 var Promise = require("bluebird"),
-    RDFTranslator = require('../../bindings/rdf-translator/app'),
-    BagIt = require('../../bindings/bagit/app'),
-    fs = Promise.promisifyAll(require('fs')),
-    path = require('path'),
-    mkdirp = require('mkdirp'),
-    _ = require('lodash');
+RDFTranslator = require('../../bindings/rdf-translator/app'),
+BagIt = require('../../bindings/bagit/app'),
+fs = Promise.promisifyAll(require('fs')),
+path = require('path'),
+mkdirp = require('mkdirp'),
+_ = require('lodash');
 
 var homeDir = '/tmp' ;
 
@@ -20,11 +20,11 @@ function newFolderStructure(opts) {
     console.log("ConverterControler::Creating directories");
 
     mkdirp(opts.masterPath, function(err) {
-        mkdirp(opts.derivativePath, function(err) {
-            mkdirp(opts.sourceMDPath, function(err) {
-                resolve(opts);
-              });
+      mkdirp(opts.derivativePath, function(err) {
+        mkdirp(opts.sourceMDPath, function(err) {
+          resolve(opts);
         });
+      });
     });
   });
 
@@ -49,38 +49,38 @@ function symLinkToIFC(opts) {
 function symLinktoDerivates(opts) {
 
   return Promise.each(opts.digitalObject.derivatives, function (derivativeObject) {
-      return new Promise(function(resolve, reject) {
+    return new Promise(function(resolve, reject) {
 
-        console.log("ConverterControler::Symlink to derivates");
+      console.log("ConverterControler::Symlink to derivates");
 
-            var derivatesSourceFilePath = derivativeObject.path;
-            var derivatesTargetFilePath = path.join(opts.derivativePath, path.basename(derivatesSourceFilePath));
+      var derivatesSourceFilePath = derivativeObject.path;
+      var derivatesTargetFilePath = path.join(opts.derivativePath, path.basename(derivatesSourceFilePath));
 
-            fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
-              resolve(opts);
-            });
-        });
-    }).then(function () {
-      return opts;
+      fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
+        resolve(opts);
+      });
     });
+  }).then(function () {
+    return opts;
+  });
 }
 
 function createBuilMXML(opts) {
   return new Promise(function(resolve, reject) {
 
 
-  console.log("ConverterControler::Creating buildm.xml");
-  var rdf = new RDFTranslator();
+    console.log("ConverterControler::Creating buildm.xml");
+    var rdf = new RDFTranslator();
 
-  rdf.extractFromJSONLD(opts.buildm, "json-ld", "xml", function(data) {
+    rdf.extractFromJSONLD(opts.buildm, "json-ld", "xml", function(data) {
       console.log("ConverterControler::Writing buildm.xml");
 
 
       fs.writeFile(path.join(opts.sourceMDPath, 'buildm.xml'), data, function(err) {
         resolve(opts);
       });
+    });
   });
-});
 }
 
 function createBagIt(bagItOpts, cb) {
@@ -95,113 +95,114 @@ function createBagIt(bagItOpts, cb) {
 }
 
 module.exports = {
-    create: function(req, res, next) {
-        var body = req.body;
-        var session = body.sessions[0];
-        var physicalAsset = session.physicalAssets[0];
-        var buildm = session.buildm;
+  create: function(req, res, next) {
+    var body = req.body;
+    var session = body.sessions[0];
+    var physicalAsset = session.physicalAssets[0];
+    var buildm = session.buildm;
 
-        var pa = _.filter(buildm, function(instance) {
-            //console.log(JSON.stringify(instance, null, 4));
-            if (instance['@type'].length){
-            	console.log(JSON.stringify(instance['@type'][0], null, 4));
-                return instance['@type'][0] == "http://data.duraark.eu/vocab/PhysicalAsset";
-            }
-        });
-        if (!pa) {
-            return res.send(500, 'Could not find pysical asset in session');
-        }
-
-
-        var sessionPath = path.join(homeDir, 'session_' + path.basename(pa[0]['@id']));
-
-        var bagItOpts ={
-          source: sessionPath,
-          target: path.join(homeDir, 'bag.zip')
-        };
-
-        Promise.each(physicalAsset.digitalObjects, function(digitalObject, index, value){
-
-          var folder = index + 1;
-          var sipPath = path.join(sessionPath, 'ie_id' + folder);
-
-          var opts = {
-           buildm : buildm,
-           digitalObject: digitalObject,
-           sipPath : sipPath,
-           masterPath : path.join(sipPath, 'master'),
-           derivativePath : path.join(sipPath, 'derivative_copy'),
-           sourceMDPath : path.join(sipPath, 'sourcemd'),
-          };
-
-
-
-          return newFolderStructure(opts)
-          .then(symLinkToIFC)
-          .then(symLinktoDerivates)
-          .then(createBuilMXML);
-        }).then(function () {
-          if(body.output.type =='bag')
-          {
-            createBagIt(bagItOpts,function () {
-              res.send(200, 'bagged it');
-            });
-          }
-        });
-
-        /*_.forEach(physicalAsset.digitalObjects, function(da, key) {
-            var folder = key + 1;
-            var sipPath = path.join(sessionPath, 'ie_id' + folder);
-            var masterPath = path.join(sipPath, 'master');
-            var derivativePath = path.join(sipPath, 'derivative_copy');
-            var sourceMDPath = path.join(sipPath, 'sourcemd');
-
-            //create needed directories
-            console.log("ConverterControler::Creating directories");
-            mkdirp(masterPath, function(err) {
-                mkdirp(derivativePath, function(err) {
-                    mkdirp(sourceMDPath, function(err) {
-                        console.log("ConverterControler::Symlink to IFC");
-
-                        var sourceIFCPath = da.path;
-                        var targetIFCPath = path.join(masterPath, path.basename(sourceIFCPath));
-
-                        fs.link(sourceIFCPath, targetIFCPath, function() {
-                            console.log("ConverterControler::Symlink to derivates");
-                            _.forEach(da.derivatives, function(n, key) {
-
-                                var derivatesSourceFilePath = n.path;
-                                var derivatesTargetFilePath = path.join(derivativePath, path.basename(derivatesSourceFilePath));
-
-                                fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
-
-                                });
-                            });
-
-                            console.log("ConverterControler::Createing buildm.xml");
-                            var rdf = new RDFTranslator();
-
-                            rdf.extractFromJSONLD(buildm, "json-ld", "xml", function(data) {
-                                console.log("ConverterControler::Writing buildm.xml");
-
-                                fs.writeFile(path.join(sourceMDPath, 'buildm.xml'), data, function(err) {
-                                  //return resolve();
-                                    //res.send(data);
-
-                                });
-                            });
-
-                        });
-                    });
-                });
-            });
-        }).then(function(){
-
-          console.log("ConverterControler::bag it");
-          var bagit = new BagIt('/tmp/session_physicalasset_d86c761c42e440659a8a5b945f695b76', '/tmp/bag.zip');
-          bagit.bagIt(function(){
-
-          });
-        });*/
+    var pa = _.filter(buildm, function(instance) {
+      //console.log(JSON.stringify(instance, null, 4));
+      if (instance['@type'].length){
+        console.log(JSON.stringify(instance['@type'][0], null, 4));
+        return instance['@type'][0] == "http://data.duraark.eu/vocab/PhysicalAsset";
+      }
+    });
+    if (!pa) {
+      return res.send(500, 'Could not find pysical asset in session');
     }
+
+
+    var sessionPath = path.join(homeDir, 'session_' + path.basename(pa[0]['@id']));
+
+    var bagItOpts ={
+      source: sessionPath,
+      target: path.join(homeDir, 'bag.zip')
+    };
+
+    Promise.each(physicalAsset.digitalObjects, function(digitalObject, index, value){
+
+      var folder = index + 1;
+      var sipPath = path.join(sessionPath, 'ie_id' + folder);
+
+      var opts = {
+        buildm : buildm,
+        digitalObject: digitalObject,
+        sipPath : sipPath,
+        masterPath : path.join(sipPath, 'master'),
+        derivativePath : path.join(sipPath, 'derivative_copy'),
+        sourceMDPath : path.join(sipPath, 'sourcemd'),
+      };
+
+
+
+      return newFolderStructure(opts)
+      .then(symLinkToIFC)
+      .then(symLinktoDerivates)
+      .then(createBuilMXML);
+
+    }).then(function () {
+      if(body.output.type =='bag')
+      {
+        createBagIt(bagItOpts,function () {
+          res.send(200, 'bagged it');
+        });
+      }
+    });
+
+    /*_.forEach(physicalAsset.digitalObjects, function(da, key) {
+    var folder = key + 1;
+    var sipPath = path.join(sessionPath, 'ie_id' + folder);
+    var masterPath = path.join(sipPath, 'master');
+    var derivativePath = path.join(sipPath, 'derivative_copy');
+    var sourceMDPath = path.join(sipPath, 'sourcemd');
+
+    //create needed directories
+    console.log("ConverterControler::Creating directories");
+    mkdirp(masterPath, function(err) {
+    mkdirp(derivativePath, function(err) {
+    mkdirp(sourceMDPath, function(err) {
+    console.log("ConverterControler::Symlink to IFC");
+
+    var sourceIFCPath = da.path;
+    var targetIFCPath = path.join(masterPath, path.basename(sourceIFCPath));
+
+    fs.link(sourceIFCPath, targetIFCPath, function() {
+    console.log("ConverterControler::Symlink to derivates");
+    _.forEach(da.derivatives, function(n, key) {
+
+    var derivatesSourceFilePath = n.path;
+    var derivatesTargetFilePath = path.join(derivativePath, path.basename(derivatesSourceFilePath));
+
+    fs.link(derivatesSourceFilePath, derivatesTargetFilePath, function() {
+
+  });
+});
+
+console.log("ConverterControler::Createing buildm.xml");
+var rdf = new RDFTranslator();
+
+rdf.extractFromJSONLD(buildm, "json-ld", "xml", function(data) {
+console.log("ConverterControler::Writing buildm.xml");
+
+fs.writeFile(path.join(sourceMDPath, 'buildm.xml'), data, function(err) {
+//return resolve();
+//res.send(data);
+
+});
+});
+
+});
+});
+});
+});
+}).then(function(){
+
+console.log("ConverterControler::bag it");
+var bagit = new BagIt('/tmp/session_physicalasset_d86c761c42e440659a8a5b945f695b76', '/tmp/bag.zip');
+bagit.bagIt(function(){
+
+});
+});*/
+}
 };
--- bindings/sip-generator.js
+++ bindings/sip-generator.js
@@ -1,242 +1,241 @@
 var JsonSchemaConverter = require('./json-schema-converter'),
-    spawn = require('child_process').spawn,
-    path = require('path'),
-    fs = require('fs'),
-    sqlite3 = require('sqlite3').verbose(),
-    _ = require('underscore');
+  spawn = require('child_process').spawn,
+  path = require('path'),
+  fs = require('fs'),
+  sqlite3 = require('sqlite3').verbose(),
+  _ = require('underscore');
 
 var SIPGenerator = module.exports = function(opts) {
-    this._appRoot = opts.appRoot;
+  this._appRoot = opts.appRoot;
 
-    console.log('[SIPGenerator::ctor] appRoot: ' + this._appRoot);
-    console.log('[SIPGenerator::ctor] dbPath: ' + opts.dbPath);
+  console.log('[SIPGenerator::ctor] appRoot: ' + this._appRoot);
+  console.log('[SIPGenerator::ctor] dbPath: ' + opts.dbPath);
 
-    this._db = new sqlite3.Database(opts.dbPath);
+  this._db = new sqlite3.Database(opts.dbPath);
 }
 
 SIPGenerator.prototype.archive = function(sip, finish_cb) {
-    this.dump(sip);
-    this._ingestToRosetta(sip, finish_cb); // FIXXME: make thenable!
+  this.dump(sip);
+  this._ingestToRosetta(sip, finish_cb); // FIXXME: make thenable!
 }
 
 SIPGenerator.prototype.dump = function(sip) {
-    var pa = sip.physicalAsset;
+  var pa = sip.physicalAsset;
 
-    console.log('\nStarting SIP ingestion to ROSETTA ...');
+  console.log('\nStarting SIP ingestion to ROSETTA ...');
 
-    console.log('\n-------------------------------------------------------------------------');
-    console.log('CONTENT:');
-    console.log('-------------------------------------------------------------------------');
+  console.log('\n-------------------------------------------------------------------------');
+  console.log('CONTENT:');
+  console.log('-------------------------------------------------------------------------');
 
-    console.log('\n# Physical Asset: ' + sip.label)
+  console.log('\n# Physical Asset: ' + sip.label)
 
-    _.each(pa.digitalObjects, function(da, idx) {
-        console.log('\n  + DigitalObject: ' + idx);
-        console.log('          Filename: ' + da.techMD.filename);
-        console.log('          Path:     ' + da.path);
+  _.each(pa.digitalObjects, function(da, idx) {
+    console.log('\n  + DigitalObject: ' + idx);
+    console.log('          Filename: ' + da.techMD.filename);
+    console.log('          Path:     ' + da.path);
 
-        _.each(da.derivatives, function(der, idx) {
-            console.log('\n      > Derivative ' + idx);
-            console.log('              Filename: ' + der.techMD.filename);
-            console.log('              Path:     ' + der.path);
-        });
-
-        var techMD = da.techMD.filename ? true : false;
-        var descMD = da.descMD.creator ? true : false;
-        var semMD = (da.semMD.selection.length > 0) ? true : false;
-
-        console.log('\n  + techMD: ' + techMD);
-        console.log('\n  + descMD: ' + descMD);
-        console.log('\n  + semMD: ' + semMD);
+    _.each(da.derivatives, function(der, idx) {
+      console.log('\n      > Derivative ' + idx);
+      console.log('              Filename: ' + der.techMD.filename);
+      console.log('              Path:     ' + der.path);
     });
 
-    console.log('\n-------------------------------------------------------------------------\n');
+    var techMD = da.techMD.filename ? true : false;
+    var descMD = da.descMD.creator ? true : false;
+    var semMD = (da.semMD.selection.length > 0) ? true : false;
+
+    console.log('\n  + techMD: ' + techMD);
+    console.log('\n  + descMD: ' + descMD);
+    console.log('\n  + semMD: ' + semMD);
+  });
+
+  console.log('\n-------------------------------------------------------------------------\n');
 };
 
 SIPGenerator.prototype._ingestToRosetta = function(sip, finish_cb) {
-    var pa = sip.physicalAsset,
-        buildm = {}; // Stores buildm metadata in suitable form to export XML version via JSON2XML
+  var pa = sip.physicalAsset,
+    buildm = {}; // Stores buildm metadata in suitable form to export XML version via JSON2XML
 
-    console.log('\n[STEP] Generating METS description for Intellectual Entity: ' + pa.descMD.address);
+  console.log('\n[STEP] Generating METS description for Intellectual Entity: ' + pa.descMD.address);
 
-    console.log('\n-------------------------------------------------------------------------');
-    console.log('METS content:');
-    console.log('-------------------------------------------------------------------------');
+  console.log('\n-------------------------------------------------------------------------');
+  console.log('METS content:');
+  console.log('-------------------------------------------------------------------------');
 
-    var physicalAsset_XML = JsonSchemaConverter.mapToPhysicalAssetXML(pa.descMD);
+  var physicalAsset_XML = JsonSchemaConverter.mapToPhysicalAssetXML(pa.descMD);
 
-    console.log(JSON.stringify(physicalAsset_XML, null, 4));
+  console.log(JSON.stringify(physicalAsset_XML, null, 4));
 
 
 
-    console.log('\n[STEP] Generating SIP for each digital object ...');
+  console.log('\n[STEP] Generating SIP for each digital object ...');
 
-    console.log('\n-------------------------------------------------------------------------');
-    console.log('SIP Generation:');
-    console.log('-------------------------------------------------------------------------');
+  console.log('\n-------------------------------------------------------------------------');
+  console.log('SIP Generation:');
+  console.log('-------------------------------------------------------------------------');
 
-    _.each(pa.digitalObjects, function(da, idx) {
-        console.log('     Processing file: ' + da.techMD.filename);
-        console.log('\n        + Generating technical metadata description ... ');
+  _.each(pa.digitalObjects, function(da, idx) {
+    console.log('     Processing file: ' + da.techMD.filename);
+    console.log('\n        + Generating technical metadata description ... ');
 
-        var techMD_XML = JsonSchemaConverter.mapToDigitalObjectTechnicalMDXML(da.techMD);
-        console.log(JSON.stringify(techMD_XML, null, 4));
+    var techMD_XML = JsonSchemaConverter.mapToDigitalObjectTechnicalMDXML(da.techMD);
+    console.log(JSON.stringify(techMD_XML, null, 4));
 
-        console.log('\n        + Generating descriptive metadata description ... ');
+    console.log('\n        + Generating descriptive metadata description ... ');
 
-        var descMD_XML = JsonSchemaConverter.mapToDigitalObjectDescriptiveMDXML(da.descMD);
-        console.log(JSON.stringify(descMD_XML, null, 4));
+    var descMD_XML = JsonSchemaConverter.mapToDigitalObjectDescriptiveMDXML(da.descMD);
+    console.log(JSON.stringify(descMD_XML, null, 4));
 
-        console.log('\n        + Generating semantic enrichment description ... ');
+    console.log('\n        + Generating semantic enrichment description ... ');
 
-        var semMD_RDF = JsonSchemaConverter.mapToDigitalObjectSemEnrichmentRDF(da.semMD);
-        console.log(JSON.stringify(semMD_RDF, null, 4));
-    });
+    var semMD_RDF = JsonSchemaConverter.mapToDigitalObjectSemEnrichmentRDF(da.semMD);
+    console.log(JSON.stringify(semMD_RDF, null, 4));
+  });
 
-    setTimeout(finish_cb, 1000);
+  setTimeout(finish_cb, 1000);
 
-    // var buildm = {
-    //     creOrgNamn: 'DURAARK Consortium',
-    //     archiverOrganizationName: 'DURAARK Consortium',
-    //     archiverSoftwareName: 'DURAARK Workbench SIP Generator'
-    // };
+  // var buildm = {
+  //     creOrgNamn: 'DURAARK Consortium',
+  //     archiverOrganizationName: 'DURAARK Consortium',
+  //     archiverSoftwareName: 'DURAARK Workbench SIP Generator'
+  // };
 
-    // var mets = this._mapMetsFromBuildm(sip.uuid, buildm);
+  // var mets = this._mapMetsFromBuildm(sip.uuid, buildm);
 
-    // this._updateMetsData(mets, sip.uuid, function() {
-    //     this._copyFiles(sip.files, function(err) {
-    //         if (err) {
-    //             console.log('[SIPGenerator::_copyFiles] error: ' + err);
-    //             console.log('ABORTING SIP generation!');
-    //             return;
-    //         }
+  // this._updateMetsData(mets, sip.uuid, function() {
+  //     this._copyFiles(sip.files, function(err) {
+  //         if (err) {
+  //             console.log('[SIPGenerator::_copyFiles] error: ' + err);
+  //             console.log('ABORTING SIP generation!');
+  //             return;
+  //         }
 
-    //         this._createSIP(sip, finish_cb);
-    //     }.bind(this));
-    // }.bind(this));
+  //         this._createSIP(sip, finish_cb);
+  //     }.bind(this));
+  // }.bind(this));
 };
 
 SIPGenerator.prototype._mapMetsFromBuildm = function(uuid, buildm) {
 
-    return {
-        paketuid: uuid,
-        metsObjId: 'placeholder',
-        metsId: 'placeholder',
-        metsLabel: 'placeholder',
-        metsType: 'placeholder',
-        creOrgNamn: buildm.creator,
-        creIndNamn: 'placeholder',
-        creIndTelefon: 'placeholder',
-        creIndMail: 'placeholder',
-        creSoftwareNamn: 'placeholder',
-        arcOrgNamn: buildm.archiverOrganizationName,
-        arcOrgOrgNr: 'placeholder',
-        arcSoftwareNamn: buildm.archiverSoftwareName,
-        preOrgNamn: 'placeholder',
-        preOrgOrgNr: 'placeholder',
-        altRecDelType: 'placeholder',
-        altRecDelSpec: 'placeholder',
-        altRecStartDate: 'placeholder',
-        altRecSubAgr: 'placeholder',
-        metsDocId: 'placeholder'
-    }
+  return {
+    paketuid: uuid,
+    metsObjId: 'placeholder',
+    metsId: 'placeholder',
+    metsLabel: 'placeholder',
+    metsType: 'placeholder',
+    creOrgNamn: buildm.creator,
+    creIndNamn: 'placeholder',
+    creIndTelefon: 'placeholder',
+    creIndMail: 'placeholder',
+    creSoftwareNamn: 'placeholder',
+    arcOrgNamn: buildm.archiverOrganizationName,
+    arcOrgOrgNr: 'placeholder',
+    arcSoftwareNamn: buildm.archiverSoftwareName,
+    preOrgNamn: 'placeholder',
+    preOrgOrgNr: 'placeholder',
+    altRecDelType: 'placeholder',
+    altRecDelSpec: 'placeholder',
+    altRecStartDate: 'placeholder',
+    altRecSubAgr: 'placeholder',
+    metsDocId: 'placeholder'
+  }
 };
 
 SIPGenerator.prototype._updateMetsData = function(mets, uuid, cb) {
-    console.log('[SIPGenerator::_updateMetsData] inserting uuid: ' + uuid);
+  console.log('[SIPGenerator::_updateMetsData] inserting uuid: ' + uuid);
 
-    this._db.run('INSERT INTO manuellinfo(paketuid, metsObjId, metsId, metsLabel, metsType, creOrgNamn, \
+  this._db.run('INSERT INTO manuellinfo(paketuid, metsObjId, metsId, metsLabel, metsType, creOrgNamn, \
                                                                            creIndNamn, creIndTelefon, creIndMail, creSoftwareNamn, arcOrgNamn, \
                                                                            arcOrgOrgNr, arcSoftwareNamn, preOrgNamn, preOrgOrgNr, altRecDelType, \
                                                                            altRecDelSpec, altRecStartDate, altRecSubAgr, metsDocId) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        uuid, mets.metsObjId, mets.metsId, mets.metsLabel, mets.metsType, mets.creOrgNamn, mets.creIndNamn, mets.creIndTelefon,
-        mets.creIndMail, mets.creSoftwareNamn, mets.arcOrgNamn, mets.OrgOrgNr, mets.arcSoftwareNamn, mets.preOrgNamn,
-        mets.preOrgOrgNr, mets.altRecDelType, mets.altRecDelSpec, mets.altRecStartDate, mets.altRecSubAgr, mets.metsDocId, cb);
+    uuid, mets.metsObjId, mets.metsId, mets.metsLabel, mets.metsType, mets.creOrgNamn, mets.creIndNamn, mets.creIndTelefon,
+    mets.creIndMail, mets.creSoftwareNamn, mets.arcOrgNamn, mets.OrgOrgNr, mets.arcSoftwareNamn, mets.preOrgNamn,
+    mets.preOrgOrgNr, mets.altRecDelType, mets.altRecDelSpec, mets.altRecStartDate, mets.altRecSubAgr, mets.metsDocId, cb);
 }
 
 SIPGenerator.prototype._copyFiles = function(files, finish_cb) {
-    console.log('[SIPGenerator::_copyFiles] copying files...');
+  console.log('[SIPGenerator::_copyFiles] copying files...');
 
-    var idx_file = 0,
-        appRoot = this._appRoot,
-        copyFile = this._copyFile;
+  var idx_file = 0,
+    appRoot = this._appRoot,
+    copyFile = this._copyFile;
 
-    function handleFile(idx, files) {
-        console.log('[SIPGenerator::handleFile]  files: ' + JSON.stringify(files, null, 4));
-        if (idx <= files.length) {
-            if (idx === files.length) {
-                finish_cb();
-                return;
-            }
+  function handleFile(idx, files) {
+    console.log('[SIPGenerator::handleFile]  files: ' + JSON.stringify(files, null, 4));
+    if (idx <= files.length) {
+      if (idx === files.length) {
+        finish_cb();
+        return;
+      }
 
-            var file = files[idx++],
-                source = file.path,
-                target = path.join(appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'built', 'content', path.basename(file.path));
+      var file = files[idx++],
+        source = file.path,
+        target = path.join(appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'built', 'content', path.basename(file.path));
 
-            copyFile(source, target, handleFile.bind(this, idx, files));
-        }
+      copyFile(source, target, handleFile.bind(this, idx, files));
     }
+  }
 
-    if (files.length) {
-        handleFile(0, files);
-    }
+  if (files.length) {
+    handleFile(0, files);
+  }
 };
 
 SIPGenerator.prototype._createSIP = function(sip, finish_cb) {
-    console.log('[SIPGenerator::_createSIP] creating SIP...');
+  console.log('[SIPGenerator::_createSIP] creating SIP...');
 
-    // java -jar server/executables/sipgen/SIP_Generator/run/eARDsip.jar 6e2e1358-f979-4c1d-afb3-09635b575370
+  // java -jar server/executables/sipgen/SIP_Generator/run/eARDsip.jar 6e2e1358-f979-4c1d-afb3-09635b575370
 
-    var exec_path = 'java',
-        sip_jar = path.join(this._appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'run', 'eARDsip.jar'),
-        sip_path = path.join(this._appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'sip', sip.uuid + '.zip'),
-        public_path = path.join(this._appRoot, '.tmp', 'public', sip.uuid + '.zip'),
-        options = '-jar ' + sip_jar + ' ' + sip.uuid,
-        that = this;
+  var exec_path = 'java',
+    sip_jar = path.join(this._appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'run', 'eARDsip.jar'),
+    sip_path = path.join(this._appRoot, 'server', 'executables', 'sipgen', 'SIP_Generator', 'sip', sip.uuid + '.zip'),
+    public_path = path.join(this._appRoot, '.tmp', 'public', sip.uuid + '.zip'),
+    options = '-jar ' + sip_jar + ' ' + sip.uuid,
+    that = this;
 
-    console.log('[SIPGenerator::identify] About to execute: ' + exec_path + ' ' + options);
+  console.log('[SIPGenerator::identify] About to execute: ' + exec_path + ' ' + options);
 
-    var bla = options.split(' ');
+  var bla = options.split(' ');
 
-    var executable = spawn(exec_path, bla);
+  var executable = spawn(exec_path, bla);
 
-    executable.stdout.on('data', function(data) {
-        console.log(data.toString());
-    });
+  executable.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
 
-    executable.stderr.on('data', function(data) {
-        console.log('[SIPGenerator::_createSIP] error: ' + data);
-        console.log(data.toString());
-    });
-
-    executable.on('close', function() {
-        that._copyFile(sip_path, public_path, function() {
-            var relative_www_path = '/' + sip.uuid + '.zip';
-            finish_cb(relative_www_path);
-        })
+  executable.stderr.on('data', function(data) {
+    console.log('[SIPGenerator::_createSIP] error: ' + data);
+    console.log(data.toString());
+  });
+  executable.on('close', function() {
+    that._copyFile(sip_path, public_path, function() {
+      var relative_www_path = '/' + sip.uuid + '.zip';
+      finish_cb(relative_www_path);
     })
+  })
 };
 
 SIPGenerator.prototype._copyFile = function(source, target, cb) {
-    console.log('[SIPGenerator::_copyFile] source: ' + source);
-    console.log('[SIPGenerator::_copyFile] target: ' + target);
+  console.log('[SIPGenerator::_copyFile] source: ' + source);
+  console.log('[SIPGenerator::_copyFile] target: ' + target);
 
-    var cbCalled = false;
+  var cbCalled = false;
 
-    var rd = fs.createReadStream(source);
-    rd.on("error", done);
+  var rd = fs.createReadStream(source);
+  rd.on("error", done);
 
-    var wr = fs.createWriteStream(target);
-    wr.on("error", done);
-    wr.on("close", function(ex) {
-        done();
-    });
-    rd.pipe(wr);
+  var wr = fs.createWriteStream(target);
+  wr.on("error", done);
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
 
-    function done(err) {
-        if (!cbCalled) {
-            cb(err);
-            cbCalled = true;
-        }
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
     }
+  }
 }
